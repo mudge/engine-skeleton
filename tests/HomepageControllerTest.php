b8f2a8ed8e45b97e4e937b99c95332c801d7b096
Paul Mucur <mudge@mudge.name>
2018-01-01 16:07:17 +0000

    Reduce noise in the example controller test

--- tests/HomepageControllerTest.php
+++ tests/HomepageControllerTest.php
@@ -4,22 +4,30 @@ declare(strict_types=1);
 namespace App;
 
 use PHPUnit\Framework\TestCase;
-use Psr\Log\NullLogger;
-use Engine\{TestRequest, TestResponse, ResponseInterface};
+use Engine\{TestRequest, TestResponse, RequestInterface, ResponseInterface};
 
 final class HomepageControllerTest extends TestCase
 {
     public function testIndexRendersWelcomePage(): void
     {
-        $logger = new NullLogger();
-        $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(__DIR__ . '/../templates'));
+        $request = $this->buildRequest('GET', '/');
+        $response = $this->buildResponse();
 
-        $request = new TestRequest('GET', '/');
-        $response = new TestResponse($twig, $logger);
-
-        $controller = new HomepageController($request, $response, $logger);
+        $controller = new HomepageController($request, $response);
         $controller->index();
 
         $this->assertContains('Huzzah!', $response->body);
+    }
+
+    private function buildRequest(string $method, string $uri): RequestInterface
+    {
+        return new TestRequest($method, $uri);
+    }
+
+    private function buildResponse(): ResponseInterface
+    {
+        $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(__DIR__ . '/../templates'));
+
+        return new TestResponse($twig);
     }
 }
